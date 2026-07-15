98f62469eec16acca46663dc77be0122fea31e8b
Sowmya S Gowda <sowmyasgowda27@gmail.com>
2026-07-15 14:44:49 +0200

    Format Terraform storage configuration

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,11 +1,11 @@
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "stgiaclearning2026"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
+  name                = "stgiaclearning2026"
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  https_traffic_only_enabled = true
+  https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
 }

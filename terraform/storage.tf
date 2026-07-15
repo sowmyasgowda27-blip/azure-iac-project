466219044f401d9d23fd119a6454f97d282f3075
Sowmya S Gowda <sowmyasgowda27@gmail.com>
2026-07-15 14:27:18 +0200

    create new storage account

---
+++ terraform/storage.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "stgiaclearning2026"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  https_traffic_only_enabled = true
+  allow_nested_items_to_be_public = false
+}
